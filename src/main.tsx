78a6449466c5bb442642f740a4cd410d18b4f533
Mahsa <mahsa.shabaninezhad@gmail.com>
2023-09-02 23:28:22 +0330

    feat: add Apollo

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,25 @@ import "./index.css";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./Theme.tsx";
 import { BrowserRouter } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'https://rickandmortyapi.graphcdn.app/',
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ApolloProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
