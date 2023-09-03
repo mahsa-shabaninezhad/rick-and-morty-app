e84b3531667d027b03b8d2c58da95bd0deceed38
Mahsa <mahsa.shabaninezhad@gmail.com>
2023-09-03 20:50:18 +0330

    feat: add pagination  for characters

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,27 @@ import "./index.css";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./Theme.tsx";
 import { BrowserRouter } from "react-router-dom";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'https://rickandmortyapi.graphcdn.app/',
-  cache: new InMemoryCache(),
+  uri: "https://rickandmortyapi.graphcdn.app/",
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          characters: {
+            keyArgs: ["filter"],
+            merge(existing = { info: {}, results: [] }, incoming = []) {
+              return {
+                info: incoming.info,
+                results: [...existing.results, ...incoming.results],
+              };
+            },
+          },
+        },
+      },
+    },
+  }),
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
