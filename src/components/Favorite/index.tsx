99513d24cf3838c135bbcce755d7b6e263fb27b9
Mahsa <mahsa.shabaninezhad@gmail.com>
2023-09-03 20:45:39 +0330

    feat: add favorite component

---
+++ src/components/Favorite/index.tsx
@@ -0,0 +1,34 @@
+import { ReactComponent as FielledFavoriteIcon } from "../../assets/icons/favorite-filled.svg";
+import { ReactComponent as FavoriteIcon } from "../../assets/icons/favorite.svg";
+import { useState } from "react";
+import { theme } from "../../Theme";
+import { Box } from "@mui/material";
+
+function Favorite({ type }: { type: 'character' | 'episode' }) {
+  const [isFavorite, setIsFavorite] = useState(false);
+  function handleFavoriteChange() {
+    setIsFavorite((prevState) => !prevState);
+  }
+
+  return (
+    <Box sx={{ flexShrink: 0 }}>
+      {isFavorite ? (
+        <FielledFavoriteIcon
+          onClick={handleFavoriteChange}
+          width={48}
+          height={48}
+          fill={theme.palette.primary.main}
+        />
+      ) : (
+        <FavoriteIcon
+          onClick={handleFavoriteChange}
+          width={48}
+          height={48}
+          fill="#fff"
+        />
+      )}
+    </Box>
+  );
+}
+
+export default Favorite;
